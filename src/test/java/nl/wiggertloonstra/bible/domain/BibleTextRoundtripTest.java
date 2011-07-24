5a0e209637378e18ef927f94077021fcbcfa5451
Wiggert <wiggertloonstra@gmail.com>
2011-07-24 12:14:47 +0200

    added rollback=true to integration test to not have de db changed after running test

--- src/test/java/nl/wiggertloonstra/bible/domain/BibleTextRoundtripTest.java
+++ src/test/java/nl/wiggertloonstra/bible/domain/BibleTextRoundtripTest.java
@@ -14,9 +14,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:test-applicationContext.xml"})
+@TransactionConfiguration(defaultRollback=false)
 public class BibleTextRoundtripTest {
     
     @Autowired
